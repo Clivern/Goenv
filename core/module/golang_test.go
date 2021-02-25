f1b57be7631ba150f4eb59fda69a140f58809409
Clivern <hello@clivern.com>
2021-02-25 20:42:32 +0100

    init

--- core/module/golang_test.go
+++ core/module/golang_test.go
@@ -22,21 +22,27 @@ func TestUnitGolang(t *testing.T) {
 
 	g.Describe("#Golang", func() {
 		g.It("It should satisfy test cases", func() {
+			// Test GetVersions
 			g.Assert(len(goenv.GetVersions())).Equal(242)
 
+			// Test SetVersion
 			versionFile := fmt.Sprintf("%s/.goenv/%s", baseDir, ".go-version")
 			g.Assert(goenv.SetVersion(versionFile, "1.18")).Equal(nil)
 
+			// Test Configure
 			g.Assert(goenv.Configure()).Equal(nil)
 
+			// Test GetLocalVersion
 			out, err := goenv.GetLocalVersion(fmt.Sprintf("%s/.goenv", baseDir))
 			g.Assert(out).Equal("1.18")
 			g.Assert(err).Equal(nil)
 
+			// Test GetGlobalVersion
 			out, err = goenv.GetGlobalVersion()
 			g.Assert(out).Equal("1.18")
 			g.Assert(err).Equal(nil)
 
+			// Cleanup
 			goenv.FileSystem.ClearDir(fmt.Sprintf("%s/.goenv", baseDir))
 			goenv.FileSystem.DeleteDir(fmt.Sprintf("%s/.goenv", baseDir))
 		})
