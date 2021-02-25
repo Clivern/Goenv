18909e6b1b1904796e6e5a5cd74f7bc14dde3dc1
Clivern <hello@clivern.com>
2021-02-25 13:25:21 +0100

    init

--- core/module/charm.go
+++ core/module/charm.go
@@ -3,3 +3,11 @@
 // license that can be found in the LICENSE file.
 
 package module
+
+// Charm type
+type Charm struct {
+	Items    []string
+	Selected string
+	Title    string
+	Callback func(string) error
+}
