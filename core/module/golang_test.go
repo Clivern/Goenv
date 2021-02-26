d0b2a2cba933bab07293d7c9f413a050cd7e641c
Clivern <hello@clivern.com>
2021-02-26 22:30:33 +0100

    update

--- core/module/golang_test.go
+++ core/module/golang_test.go
@@ -23,7 +23,7 @@ func TestUnitGolang(t *testing.T) {
 	g.Describe("#Golang", func() {
 		g.It("It should satisfy test cases", func() {
 			// Test GetVersions
-			g.Assert(len(goenv.GetVersions())).Equal(242)
+			g.Assert(len(goenv.GetVersions())).Equal(210)
 
 			// Test SetVersion
 			versionFile := fmt.Sprintf("%s/.goenv/%s", baseDir, ".go-version")
